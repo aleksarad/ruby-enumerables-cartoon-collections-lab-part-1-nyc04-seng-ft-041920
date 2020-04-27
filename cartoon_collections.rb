3d58cdd518d0a1c82356669353314b598e38c629
Alexandra Radevich <alexa.radevich1@gmail.com>
2020-04-27 19:37:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   
-  array.each.with_index(1) do |item, index|
+  array.eachwith_index do |item, index|
     puts "#{index + 1}. #{item}"
   end
 end
